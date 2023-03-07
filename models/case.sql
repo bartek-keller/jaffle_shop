66bccf529fcdbab6fe64906985e35d5e14fd4b27
Bartek Keller <103174128+bartek-keller@users.noreply.github.com>
2023-03-07 13:43:17 +0100

    Remove obsolete models

--- models/case.sql
+++ models/case.sql
@@ -4,17 +4,15 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_orders') }}
+    select * from {{ ref('Case') }}
 
 ),
 
 renamed as (
 
     select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+        column1 as test_column,
+        column2 as Id
 
     from source
 
